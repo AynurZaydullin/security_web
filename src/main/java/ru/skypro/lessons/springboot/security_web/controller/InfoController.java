965afc7ff9d840b1670bf967655b03ad12101a7e
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-09-11 17:16:08 +0300

    Сделанное ДЗ на тему "Логирование и конфигурирование приложения".

--- src/main/java/ru/skypro/lessons/springboot/security_web/controller/InfoController.java
+++ src/main/java/ru/skypro/lessons/springboot/security_web/controller/InfoController.java
@@ -2,22 +2,13 @@ package ru.skypro.lessons.springboot.security_web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.lessons.springboot.security_web.service.Employee;
-import ru.skypro.lessons.springboot.security_web.service.EmployeeService;
-
-import java.util.List;
 
 @RestController
 public class InfoController {
-    @Value("${app.env.dev}")
-    private String dev;
-    private final EmployeeService employeeService;
+    @Value("${app.env}")
+    private final String dev = null;
 
-    public InfoController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
 
     @GetMapping("/appInfo")
     public String getAppInfo() {
